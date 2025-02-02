3fa8cbf96b50eb565b86144d5d32dc94932474f0
Gustavo Henrique Zenke <gustavozenke01@gmail.com>
2025-02-02 18:28:46 -0300

    Refatoracao para reaproveitar metodo send

--- src/application/usecase/sqs_usecase.py
+++ src/application/usecase/sqs_usecase.py
@@ -7,11 +7,6 @@ class SqsUsecase:
 
     def __init__(self):
         self.sqs_client = boto3.client('sqs')
-        self.queue_url = "https://sqs.us-east-1.amazonaws.com/369780787289/sqs-processamento"
 
-    def send(self, nome_arquivo, nome_usuario):
-        message = {
-            "nome_arquivo": nome_arquivo,
-            "nome_usuario": nome_usuario
-        }
-        return self.sqs_client.send_message(QueueUrl=self.queue_url, MessageBody=json.dumps(message))
+    def send(self, queue_url, message):
+        return self.sqs_client.send_message(QueueUrl=queue_url, MessageBody=json.dumps(message))
