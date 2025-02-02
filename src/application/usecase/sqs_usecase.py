e62564252f5c2b482d2e41cf9b6ceca959748f93
Gustavo Henrique Zenke <gustavozenke01@gmail.com>
2025-02-02 18:32:08 -0300

    Correcao ordem parametros

--- src/application/usecase/sqs_usecase.py
+++ src/application/usecase/sqs_usecase.py
@@ -8,5 +8,5 @@ class SqsUsecase:
     def __init__(self):
         self.sqs_client = boto3.client('sqs')
 
-    def send(self, queue_url, message):
+    def send(self, message, queue_url):
         return self.sqs_client.send_message(QueueUrl=queue_url, MessageBody=json.dumps(message))
