af5853930a0c5741bec072529a1a20643a048eb9
Gustavo Henrique Zenke <gustavozenke01@gmail.com>
2025-02-07 19:29:06 -0300

    retirada envio sqs inicio processamento

--- src/application/service/iniciar_processamento_service.py
+++ src/application/service/iniciar_processamento_service.py
@@ -1,13 +1,11 @@
 from application.usecase.sqs_usecase import SqsUsecase
 from application.usecase.video_usecase import VideoUsecase
-from domain.enums.status_processamento import StatusProcessamento
 
 
 class IniciarProcessamentoService:
     def __init__(self):
         self.video_usecase = VideoUsecase()
         self.sqs_usecase = SqsUsecase()
-        self.queue_status_url = "https://sqs.us-east-1.amazonaws.com/369780787289/sqs-gravar-status-processamento"
         self.queue_processamento_url = "https://sqs.us-east-1.amazonaws.com/369780787289/sqs-processamento"
 
     def iniciar_processamento(self, nome_video, tamanho):
@@ -15,9 +13,6 @@ class IniciarProcessamentoService:
         formato = nome_video.split('.')[1]
 
         self.video_usecase.gravar_metadados_video(nome_video, nome_usuario, formato, tamanho)
-
-        self.sqs_usecase.send(self.formatar_mensagem_queue_status(nome_video, nome_usuario),
-                              self.queue_status_url)
 
         self.sqs_usecase.send(self.formatar_mensagem_queue_processamento(nome_video, nome_usuario),
                               self.queue_processamento_url)
@@ -28,8 +23,3 @@ class IniciarProcessamentoService:
             "nome_video": nome_video,
             "nome_usuario": nome_usuario
         }
-
-    def formatar_mensagem_queue_status(self, nome_video, nome_usuario):
-        message = self.formatar_mensagem_queue_processamento(nome_video, nome_usuario)
-        message['status_processamento'] = StatusProcessamento.PROCESSAMENTO_INICIADO.name
-        return message
